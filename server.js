d7d2bc4d03b6682f9818f1ee4d583de559004d67
ascendtradingdevelopment <derrickdunville@gmail.com>
2018-11-29 20:53:47 -0500

    changing port to 80

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT ? process.env.PORT : 8080;
+const port = process.env.PORT ? process.env.PORT : 80;
 const dist = path.join(__dirname, 'dist');
 
 const api_url = process.env.API_URL ? process.env.API_URL : "http://localhost:3000/";
