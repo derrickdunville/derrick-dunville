d9d554ab9b8a38acd8df70b41490258e603256f6
ascendtradingdevelopment <derrickdunville@gmail.com>
2018-12-04 22:20:49 -0500

    Asset updates, fixed some color issues, added more skills

--- src/components/resume/Skills.js
+++ src/components/resume/Skills.js
@@ -5,11 +5,17 @@ import { Input, Button } from 'react-bootstrap'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import Skill from './Skill'
-import java_logo from '../../assets/java_logo_250x250_bw.png'
-import javascript_logo from '../../assets/javascript_logo_250x250_bw.png'
-import python_logo from '../../assets/python_logo_250x250_bw.png'
-import mssql_logo from '../../assets/mssql_logo_250x250_bw.png'
-import cplusplus_logo from '../../assets/cplusplus_logo_250x250_bw.png'
+import java_logo from '../../assets/java_logo_250x250.png'
+import javascript_logo from '../../assets/javascript_logo_250x250.png'
+import python_logo from '../../assets/python_logo_250x250.png'
+import mssql_logo from '../../assets/mssql_logo_250x250.png'
+import cplusplus_logo from '../../assets/cplusplus_logo_250x250.png'
+import office_logo from '../../assets/office_logo.png'
+import node_logo from '../../assets/node_logo.png'
+import react_logo from '../../assets/react_logo.png'
+import redux_logo from '../../assets/redux_logo.png'
+import aws_logo from '../../assets/aws_logo.png'
+import mongodb_logo from '../../assets/mongodb_logo.png'
 
 class Skills extends Component {
 
@@ -30,6 +36,12 @@ class Skills extends Component {
           <Skill title="Python" img={python_logo}/>
           <Skill title="Javascript" img={javascript_logo}/>
           <Skill title="MSSQL" img={mssql_logo} />
+          <Skill title="MongoDB" img={mongodb_logo} />
+          <Skill title="Office" img={office_logo}/>
+          <Skill title="Node" img={node_logo}/>
+          <Skill title="React" img={react_logo}/>
+          <Skill title="Redux" img={redux_logo}/>
+          <Skill title="AWS" img={aws_logo}/>
         </div>
       </section>
     );
